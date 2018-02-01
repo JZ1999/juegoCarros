cb0748b8c205a835eaa50d00d57d7506a5551f24
Joseph David Zamora Murillo <joseph.zamora64@yahoo.com>
2018-02-01 14:45:56 -0600

    Default

---
+++ Assets/Scripts/StaticRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticRotation : MonoBehaviour {
+
+	#region Variables
+	private Quaternion rot;//rotacion inicial
+	#endregion
+
+
+	#region Custom Methods
+
+	#endregion
+
+
+	#region Unity Methods
+	void Start () 
+	{
+		rot = transform.rotation;
+	}
+	
+	
+	void Update () 
+	{
+		transform.rotation = rot;
+	}
+
+	#endregion
+}
