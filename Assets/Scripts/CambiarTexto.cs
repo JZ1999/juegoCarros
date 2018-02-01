5ae27ec0c3287423fbb7c04b0981283e42c13246
Joseph David Zamora Murillo <joseph.zamora64@yahoo.com>
2018-02-01 02:31:50 -0600

    Proyecto

---
+++ Assets/Scripts/CambiarTexto.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CambiarTexto : MonoBehaviour {
+
+	#region Variables
+	public GameObject player;
+	private int puntos;
+	public Text obj;
+	private ComerItem script;
+	#endregion
+
+
+	#region Custom Methods
+
+	#endregion
+
+
+	#region Unity Methods
+	void Start () 
+	{
+		script = player.GetComponent<ComerItem>();
+		puntos = script.GetPuntos();
+	}
+	
+
+	void Update () 
+	{
+		puntos = script.GetPuntos();
+		obj.text = puntos.ToString();
+	}
+
+	#endregion
+}
---
+++ Assets/Scripts/ComerItem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComerItem : MonoBehaviour {
+
+	#region Variables
+	private int puntos;
+	public AudioClip shootSound;
+	public AudioSource source;
+	#endregion
+
+
+	#region Custom Methods
+	public int GetPuntos ()
+	{
+		return puntos;
+	}
+	#endregion
+
+
+	#region Unity Methods
+	void Start () 
+	{
+		puntos = 0;
+		source.clip = shootSound;
+	}
+	
+	
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.CompareTag ("Item_Dona")) 
+		{
+			source.Play();
+			puntos++;
+			Destroy(other.gameObject);
+			Debug.Log(puntos);
+		}
+
+	}
+
+	#endregion
+}
